e35e16e9ef5ae1f253f4a221c9600b0135f5c4b1
tark000 <tark000@ukr.net>
2013-06-08 01:40:30 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+House::Application.config.secret_token = '996c4aee8e06e379d116d36ecc4da37b7dc61fd15f7149f2193619d6f5dbb905d3d0a708915ddffcd9fc13c275dece36816a5740f8ad2e31e67a6942f45543e6'
